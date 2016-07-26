34faabe05462929a434b8b82dea4a9503394b94c
C-Elegans <mtnolan2640@gmail.com>
2016-07-26 09:55:20 -0400

    Fix pep8 error

--- d16i/io.py
+++ d16i/io.py
@@ -5,7 +5,7 @@ IO_UART_TX_REG = 0xFF02
 def write_io_8(address, data):
     if isIo(address):
         if address == IO_UART_TX_REG:
-            print(chr(data),end="")
+            print(chr(data), end="")
 
 
 def isIo(address):
